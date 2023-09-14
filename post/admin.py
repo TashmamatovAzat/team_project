e5b3c55758b78dfcb4042cd58bc320669d0bae4e
Admin <jika.baiseitova@gmail.com>
2023-09-14 11:38:04 +0600

    added admin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Post, Comment, Rating, User
 
-# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Rating)
+admin.site.register(User)
