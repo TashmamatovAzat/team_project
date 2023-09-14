f8fd661711b3a16e5f824a84f89edf0f07cbb7b1
Admin <jika.baiseitova@gmail.com>
2023-09-14 11:56:53 +0600

    user in account 2

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from . models import Post, Comment, Rating, User
-
+from . models import Post, Comment, Rating
 
 admin.site.register(Post)
 admin.site.register(Comment)
